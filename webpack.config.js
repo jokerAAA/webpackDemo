9469150f62388380391362c30f9863f9da566afb
fulei <fulei@taoshouyou.com>
2018-07-14 16:42:35 +0800

    添加uglifyjs插件

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const webpack = require("webpack");
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const CleanWebpackPlugin = require("clean-webpack-plugin");
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
 
 module.exports = {
     entry:{
@@ -12,17 +13,32 @@ module.exports = {
         path:path.join(__dirname,'./dist') 
     },
     plugins:[
-        /* 文档地址:https://github.com/johnagan/clean-webpack-plugin */
+        /**
+         * 文档地址:https://github.com/johnagan/clean-webpack-plugin 
+         * 用于清除每次构建后生成的dist文件夹
+         * */
         new CleanWebpackPlugin('dist',{
             
         }),
         new HtmlWebpackPlugin({
-            /* 具体见文档：https://github.com/jantimon/html-webpack-plugin */
+            /**
+             * 
+             *  具体见文档：https://github.com/jantimon/html-webpack-plugin
+             * 用于配置打包的bundle注入的模板文件
+             *  */
             title:"test",
             filename:"index.html",
             template:"./index.html",
             inject:true
         }),
+        /**
+         *  文档地址：https://github.com/webpack-contrib/uglifyjs-webpack-plugin
+         * 用于压缩混淆js代码
+         */
+        new UglifyJsPlugin({
+            test: /\.js($|\?)/i,
+        })
+        
     ],
     module:{
         rules:[
